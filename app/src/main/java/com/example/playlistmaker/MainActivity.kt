9562025d46ce8298fe9d68df8b43026fad1f7a69
Babaylov7 <babaylov-ayu@mail.ru>
2023-07-20 17:32:19 +0500

    20.07.23 Создание проекта Спринт 8 Тема 4

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_activity)
 
+//        val image = findViewById<ImageView>(R.id.image)
+//
+//        image.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
