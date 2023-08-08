ff33e720338d2a204838b6bc60542f77bde16de7
Babaylov7 <babaylov-ayu@mail.ru>
2023-08-08 10:43:35 +0500

    08.08.23 Спринт 9 Тема 3

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,17 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_activity)
+
+        val buttonBack = findViewById<ImageView>(R.id.button_back)
+
+        buttonBack.setOnClickListener {
+            finish()
+        }
     }
 }
