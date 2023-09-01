c7b0bec61cf8082a46a73a79e9cc4564aed79fdc
Babaylov7 <babaylov-ayu@mail.ru>
2023-09-01 14:27:06 +0500

    01.09.23 Спринт 11 ДЗ

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -6,27 +6,46 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
-    private var editTextValue = ""
 
     companion object {
         const val SEARCH_TEXT = "SEARCH_TEXT"
     }
 
+    private var editTextValue = ""
+    private val itunesBaseUrl = "https://itunes.apple.com"
+    private val tracks = ArrayList<Track>()
+    private val adapter = TrackAdapter(tracks)
+    private lateinit var buttonBack: ImageView
+    private lateinit var inputEditText: EditText
+    private lateinit var clearButton: ImageButton
+    private lateinit var recyclerView: RecyclerView
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(itunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val itunesService = retrofit.create(ItunesApi::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_activity)
 
-        val buttonBack = findViewById<ImageView>(R.id.button_back)
-        val inputEditText = findViewById<EditText>(R.id.editText)
-        val clearButton = findViewById<ImageButton>(R.id.clearButton)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        buttonBack = findViewById(R.id.button_back)
+        inputEditText = findViewById(R.id.editText)
+        clearButton = findViewById(R.id.clearButton)
+        recyclerView = findViewById(R.id.recyclerView)
 
         buttonBack.setOnClickListener {
             finish()
@@ -54,36 +73,51 @@ class SearchActivity : AppCompatActivity() {
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
 
-        val listOfTracks = listOf<Track>(           //Временная заглушка с данными
-            Track("Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-        )
+        recyclerView.adapter = adapter
 
-        val tracksAdapter = TrackAdapter(listOfTracks)
-        recyclerView.adapter = tracksAdapter
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (inputEditText.text.isNotEmpty()) {
+                    itunesService.search(inputEditText.text.toString())
+                        .enqueue(object : Callback<TrackResponse> {
+                            override fun onResponse(            //Ответ
+                                call: Call<TrackResponse>,
+                                response: Response<TrackResponse>
+                            ) {
+                                if (response.code() == 200) {
+                                    tracks.clear()
+                                    if (response.body()?.results?.isNotEmpty() == true) {
+                                        tracks.addAll(response.body()?.results!!)
+                                        adapter.notifyDataSetChanged()
+                                    }
+                                    if (tracks.isEmpty()) {
+                                        Toast.makeText(
+                                            applicationContext,
+                                            "Ничего не нашлось",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+                            }
 
-
-
-
+                            override fun onFailure(
+                                call: Call<TrackResponse>,
+                                t: Throwable
+                            ) {                                         //Возврат ошибки
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Ошибка сети",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        })
+                    true
+                }
+            }
+            false
+        }
     }
+
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
@@ -102,5 +136,6 @@ class SearchActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         editTextValue = savedInstanceState.getString(SEARCH_TEXT, "")
     }
+
 
 }
--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -2,8 +2,8 @@ package com.example.playlistmaker
 
 data class Track(
     val trackName: String,
-    val groupName: String,
-    val trackDuration: String,
+    val artistName: String,
+    val trackTimeMillis: Long,
     val artworkUrl100: String
     )
 
