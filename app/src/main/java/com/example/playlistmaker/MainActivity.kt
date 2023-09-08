99e734a5abe7b2d6a9e5f1410d8dbcc808d130ca
Babaylov7 <babaylov-ayu@mail.ru>
2023-09-08 16:16:49 +0500

    08.09.23 16:16 Спринт 12

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_activity)
+
 
         val buttonSearch = findViewById<Button>(R.id.button_search)
         val buttonLibrary = findViewById<Button>(R.id.button_library)
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -7,17 +7,25 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 
+const val SETTINGS_PREFERENCES = "settings_preferences"
+const val NIGHT_MODE_ON = "night_mode_on"
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
 
+        val sharedPrefs = getSharedPreferences(SETTINGS_PREFERENCES, MODE_PRIVATE)
+
         val buttonBack = findViewById<ImageView>(R.id.button_back)
         val sareApplicationButton = findViewById<LinearLayout>(R.id.sare_application)
         val writeToSupportButton = findViewById<LinearLayout>(R.id.write_to_support)
         val termsOfUseButton = findViewById<LinearLayout>(R.id.terms_of_use)
+        val darkThemeSwitch = findViewById<SwitchCompat>(R.id.dark_theme_switch)
+
+        darkThemeSwitch.setChecked(sharedPrefs.getBoolean(NIGHT_MODE_ON, true))
 
         buttonBack.setOnClickListener {
             finish()
@@ -46,6 +54,14 @@ class SettingsActivity : AppCompatActivity() {
             val intent =
                 Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.terms_of_link)))
             startActivity(intent)
+        }
+
+        darkThemeSwitch.setOnCheckedChangeListener { switcher, checked ->
+            sharedPrefs.edit()
+                .putBoolean(NIGHT_MODE_ON, darkThemeSwitch.isChecked())
+                .apply()
+
+            (applicationContext as App).switchTheme(checked)
         }
 
     }
